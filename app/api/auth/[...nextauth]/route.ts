7c33bee483add772fe6e69f307b97580e05e044f
Tugberk <tugberkbardak7@gmail.com>
2025-03-15 23:05:57 +0100

    deploy bug fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,10 @@
-import { authMiddleware } from "@clerk/nextjs"
+// import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/api/webhook/clerk"],
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ["/api/webhook/clerk"],
-})
+// export default authMiddleware({
+//   publicRoutes: ["/", "/api/webhook/clerk"],
+//   ignoredRoutes: ["/api/webhook/clerk"],
+// });
 
-export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
-  // for more information about configuring your Middleware
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
-
+// export const config = {
+//   matcher: ["/((?!_next|.*\\..*).*)", "/(api|trpc)(.*)"],
+// };
