6f5f1a1bb70cb10c2b31cc7d4d34d595a612729d
Tugberk <tugberkbardak7@gmail.com>
2025-03-24 16:27:32 +0100

    loading page fixed

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,6 +1,6 @@
 import { DashboardHeader } from "@/components/dashboard/dashboard-header"
 import { Skeleton } from "@/components/ui/skeleton"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { BottomNavigationSkeleton } from "@/components/dashboard/bottom-navigation-skeleton"
 
 export default function DashboardLoading() {
   return (
@@ -8,28 +8,22 @@ export default function DashboardLoading() {
       <DashboardHeader />
 
       <main className="flex-1 container py-6">
-        <Tabs defaultValue="attendance" className="w-full">
-          <TabsList className="grid w-full grid-cols-4 mb-8">
-            <TabsTrigger value="attendance">Attendance</TabsTrigger>
-            <TabsTrigger value="friends">Friends</TabsTrigger>
-            <TabsTrigger value="global">Global</TabsTrigger>
-            <TabsTrigger value="groups">Groups</TabsTrigger>
-          </TabsList>
+        <div className="space-y-6">
+          <div className="flex justify-between items-center">
+            <Skeleton className="h-8 w-48" />
+            <Skeleton className="h-10 w-40" />
+          </div>
 
-          <TabsContent value="attendance" className="space-y-4">
-            <div className="flex justify-between items-center">
-              <Skeleton className="h-8 w-48" />
-              <Skeleton className="h-10 w-40" />
-            </div>
-
-            <div className="space-y-4 mt-6">
-              <Skeleton className="h-[300px] w-full rounded-lg" />
-              <Skeleton className="h-[200px] w-full rounded-lg" />
-              <Skeleton className="h-[200px] w-full rounded-lg" />
-            </div>
-          </TabsContent>
-        </Tabs>
+          <div className="space-y-4 mt-6">
+            <Skeleton className="h-[300px] w-full rounded-lg" />
+            <Skeleton className="h-[200px] w-full rounded-lg" />
+            <Skeleton className="h-[200px] w-full rounded-lg" />
+          </div>
+        </div>
       </main>
+
+      {/* Use the skeleton version of the bottom navigation */}
+      <BottomNavigationSkeleton />
     </div>
   )
 }
---
+++ components/dashboard/bottom-navigation-skeleton.tsx
@@ -0,0 +1,16 @@
+export function BottomNavigationSkeleton() {
+    return (
+      <div className="fixed bottom-0 left-0 right-0 z-50 bg-black border-t border-gray-800">
+        <div className="flex justify-around items-center h-14">
+          {/* Five navigation items */}
+          {[...Array(5)].map((_, i) => (
+            <div key={i} className="flex flex-col items-center justify-center w-full h-full">
+              <div className="h-5 w-5 bg-gray-700 rounded-sm opacity-50" />
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+  
+  
